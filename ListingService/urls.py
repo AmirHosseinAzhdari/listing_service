054b5012d3c78641e8649e0d620f9fa7a17919c4
partizaans1 <miaretechnicaltask@gmail.com>
2022-05-28 11:08:18 +0430

    add readme.md file

--- ListingService/urls.py
+++ ListingService/urls.py
@@ -8,11 +8,11 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Optional UI:
+    # API document UI:
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/redoc/",
+        "schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
